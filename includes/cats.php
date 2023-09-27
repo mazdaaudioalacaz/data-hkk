5d9dc04da61ff5e25e68d38435955d5748b10095
parakazanacam <129620303+parakazanacam@users.noreply.github.com>
2023-09-27 14:06:14 +0300

    888

---
+++ includes/cats.php
@@ -0,0 +1,35 @@
+<li>
+<a href="/category/films">Film & Animation</a>
+</li>
+<li>
+<a href="/category/vehicles">Autos & Vehicles
+</a>
+</li>
+<li>
+<a href="/category/music">Music & Songs
+</a>
+</li>
+<li>
+<a href="/category/pets">Pets & Animals</a>
+</li>
+<li>
+<a href="/category/sports">Sports & Play</a>
+</li>
+<li>
+<a href="/category/travel">Travel & Events</a>
+</li>
+<li>
+<a href="/category/gaming">Gaming</a>
+</li>
+<li>
+<a href="/category/vlogs">People & Blogs</a>
+</li>
+<li>
+<a href="/category/comedy">Comedy & Fun</a>
+</li>
+<li>
+<a href="/category/entertainment">Entertainment</a>
+</li>
+<li>
+<a href="/category/how+to">How to & Style</a>
+</li>
---
+++ includes/download.php
@@ -0,0 +1,137 @@
+<?php
+$id_en = base64_decode($_GET['get']);
+$id = $id_en;
+$adslink = 'https://adserver.adreactor.com/servlet/view/window/url/zone?zid=40&pid=8698';
+?>
+<html><head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="HandheldFriendly" content="True" />
+<meta name="MobileOptimized" content="320" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<meta property='og:site_name' content='MajaNet - Free Download Video & MP3' />
+<meta property='og:type' content='website' />
+<meta property='og:url' content='https://majapahit.net' />
+<meta property='og:image' content='https://res.cloudinary.com/websmoe/image/upload/v1586102664/majanet/ogimages_wdtrqu.jpg' />
+<meta property='og:image:width' content='300' />
+<meta property='og:image:height' content='100' />
+<meta property='og:title' content='MajaNet - Free Download Video & MP3' />
+<meta property='og:description' content='Search for your favorite songs, movies and videos from multiple online sources and download them in the best possible quality.' />
+<meta property='og:locale' content='en_US' />
+<meta name='twitter:site' content='MajaNet - Free Download Video & MP3' />
+<meta name='twitter:creator' content='MajaNet - Free Download Video & MP3' />
+<meta name='twitter:image' content='https://res.cloudinary.com/websmoe/image/upload/v1586102664/majanet/ogimages_wdtrqu.jpg' />
+<meta name='twitter:card' content='summary_large_image' />
+<meta name='twitter:title' content='MajaNet - Free Download Video & MP3' />
+<meta name='twitter:description' content='Search for your favorite songs, movies and videos from multiple online sources and download them in the best possible quality.' />
+<link rel="apple-touch-icon" sizes="60x60" href="https://majapahit.net/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="https://majapahit.net/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="https://majapahit.net/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="https://majapahit.net/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="https://majapahit.net/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="https://majapahit.net/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="https://majapahit.net/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://majapahit.net/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192" href="https://majapahit.net/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://majapahit.net/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="https://majapahit.net/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://majapahit.net/favicon/favicon-16x16.png">
+<link rel="manifest" href="https://majapahit.net/favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#1e272e">
+<meta name="theme-color" content="#1e272e">
+<meta name="msapplication-TileImage" content="https://majapahit.net/favicon/ms-icon-144x144.png">
+<meta name="robots" content="index,follow" />
+<title>Free Download MP3 and Videos</title>
+<meta name="robots" content="noindex,follow,noodp" />
+<style>
+body {
+background: #24232A;
+font-size: 16px;
+font-style: normal;
+color: #222;
+margin: auto;}
+.nganu {
+    border: 1px solid #757575;
+    background: #3f3f48;
+    max-width: 720px;
+    margin: 5px auto 10px auto;
+    width: 100%;
+    padding: 25px;
+}
+iframe {border-radius:5px;}
+.download-iklan {
+    background-color: #757575;
+    color: #ffffff;
+    margin-bottom: 10%;
+}
+
+.download-asli {
+    background-color: #fff;
+    color: #757575;
+    margin-bottom: 10%;
+    border: 1px solid #757575;
+}
+.download-asli, .download-iklan {
+    font-family: roboto,arial,Arial;
+    cursor: pointer;
+    text-align: center;
+    display: inline-block;
+    width: 80%;
+    max-width: 280px;
+    height: 40px;
+    border-radius: 4px;
+    margin-bottom: 20px;
+    line-height: 40px;
+
+}
+.list{border-bottom:1px solid #eee;padding:8px 0;font-size:12px;display:block;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;}
+.logo {padding:5px;text-align: center;}
+#myDIV, #myDIV2 {display:none;}
+a {text-decoration:none;}
+</style>
+
+</head><body>
+<script type="text/javascript">
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "block";
+    }
+}
+function myFunction2() {
+    var x = document.getElementById("myDIV2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "block";
+    }
+}
+</script>
+
+<div class="nganu"><div class="logo">
+<a href="/" title="Generate Link Lagu"><img style="width: 180px;" src="https://res.cloudinary.com/websmoe/image/upload/v1586245772/majanet/logo-min_tgnvam.png"></a><br>
+</div><br>
+<center>
+<a class="download-iklan" target="_blank" href="<?php echo $adslink; ?>">Download Now</a>
+<br>
+<a class="download-asli" onclick="myFunction()">Download MP3</a>
+<div id="myDIV">
+<p style="text-align:center;font-size:14px;font-style:italic;color:red;font-weight:bold;">&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;<br>LINK UTAMA<br><br />
+<iframe class="button-api-frame" src="https://api.webs.moe/@api/button/mp3/<?php echo $id; ?>" width="100%" height="80" allowtransparency="true" scrolling="no" style="border:none"></iframe>
+
+<br>&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;</p>
+</div>
+<br>
+<a class="download-asli" onclick="myFunction2()">Download Video</a>
+<div id="myDIV2">
+<p style="text-align:center;font-size:14px;font-style:italic;color:red;font-weight:bold;">&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;<br><br>LINK UTAMA<br><br />
+<iframe class="button-api-frame" src="https://api.webs.moe/@api/button/videos/<?php echo $id; ?>" width="100%" height="80" allowtransparency="true" scrolling="no" style="border:none"></iframe>
+
+<br>&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;&uarr;</p>
+</div>
+</center><br />
+</div>
+</body>
+
+</html>
